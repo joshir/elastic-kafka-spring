447dc3e825ca1b48e2fa3908703e229bda4ed9e7
joshir <hareepjoshi@gmail.com>
2023-03-30 19:20:39 -0500

    setting up config server dependencies

--- orders-config-server/src/main/java/com/joshi/robin/elastic/kafka/spring/config/server/OrdersConfigServer.java
+++ orders-config-server/src/main/java/com/joshi/robin/elastic/kafka/spring/config/server/OrdersConfigServer.java
@@ -1,2 +1,13 @@
-package com.joshi.robin.elastic.kafka.spring.config.server;public class OrdersConfigServer {
+package com.joshi.robin.elastic.kafka.spring.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class OrdersConfigServer {
+  public static void main(String[] args) {
+    SpringApplication.run(OrdersConfigServer.class, args);
+  }
 }
